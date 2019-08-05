bab3301269a5ce5fc63b83c222ac9372ff348a68
protolambda <proto@protolambda.com>
2019-08-05 17:16:40 +0200

    add websocket connection, and some styling

--- src/Pages.tsx
+++ src/Pages.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {Main} from "./components/Main";
 import About from "./components/About";
+import {createStyles, Theme, WithStyles, withStyles} from '@material-ui/core/styles';
 
-const Pages: React.FC = () => {
+const styles = (theme: Theme) => createStyles({
+    root: {
+        backgroundColor: theme.palette.background.default,
+        height: '100%'
+    }
+});
+
+interface Props extends WithStyles<typeof styles> {}
+
+const Pages: React.FC<Props> = ({classes}) => {
     return (
-        <Router>
-            <Switch>
-                <Route exact path='/' component={Main}/>
-                <Route exact strict={false} path='/about' component={About}/>
-            </Switch>
-        </Router>
+        <div className={classes.root}>
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={Main}/>
+                    <Route exact strict={false} path='/about' component={About}/>
+                </Switch>
+            </Router>
+        </div>
     );
 };
 
-export default Pages;
+export default withStyles(styles)(Pages);
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,11 +5,13 @@ import CytoscapeComponent from 'react-cytoscapejs';
 import klay from 'cytoscape-klay';
 import cytoscape from "cytoscape";
 import {Paper, Typography} from "@material-ui/core";
+import ReconnectingWebSocket from "reconnecting-websocket";
 
 cytoscape.use(klay);
 
 type MainState = {
-    loaded: boolean
+    loaded: boolean,
+    wsOpen: boolean
 }
 
 const cytoStyles: Array<cytoscape.Stylesheet> = [
@@ -28,8 +30,30 @@ export class Main extends Component<{}, MainState> {
 
     private cy: CY | undefined;
 
+    onStatusWS = (open: boolean) => () => {
+        this.setState({
+            wsOpen: open
+        })
+    };
+
+    setupWS = () => {
+        const rws = new ReconnectingWebSocket('ws://localhost:4000/ws', [], {debug: true});
+        rws.binaryType = 'arraybuffer';
+        rws.addEventListener('close', this.onStatusWS(false));
+        rws.addEventListener('open', this.onStatusWS(true));
+        rws.addEventListener('message', (msg) => {
+            const buf = msg.data;
+            // TODO
+        });
+        return () => {
+            rws.close();
+        };
+    };
+
     makeCyRef = (cy: CY) => {
         this.cy = cy;
+        // TODO: reload data from WS on CY reset.
+        console.log("reset CY instance");
     };
 
     layout = () => {
